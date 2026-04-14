d5db5ac62b2416e2b7c4beb145b70c87d1d80143
Tomi <belangertomas@gmail.com>
2026-04-14 16:49:49 -0300

    Delete Diccionario_Simple_Estatico directory

--- Diccionario_Simple_Estatico/Diccionario_Simple/src/Interface/DiccionarioSimpleStringInt.java
+++
@@ -1,8 +0,0 @@
-package Interface;
-
-public interface DiccionarioSimpleStringInt {
-    void InicializarDiccionario();
-    void Agregar(String clave, int valor);
-    void Eliminar(String clave);
-    String[] Claves();
-}
--- Diccionario_Simple_Estatico/Diccionario_Simple/src/Interface/DiccionarioSimpleStringString.java
+++
@@ -1,8 +0,0 @@
-package Interface;
-
-public interface DiccionarioSimpleStringString {
-    void InicializarDiccionario();
-    void Agregar(String clave, String valor);
-    void Eliminar(String clave);
-    String[] Claves();
-}
